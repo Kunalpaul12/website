fb2fb72eb65ef55395046d0d02c3eb0913390599
kunal <kunalp@invezta.com>
2020-04-05 05:02:42 +0530

    skills ui

---
+++ src/screens/skills/style.js
@@ -0,0 +1,37 @@
+export default {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    width: "85%",
+    border: "1px solid white",
+    borderRadius: 10,
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginTop: "2%",
+  },
+  titile_text: {
+    color: "white",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "1%",
+    fontSize: 20,
+  },
+  content_web_view: {
+    display: "flex",
+    width: "95%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    //border: "1px solid white",
+  },
+  content_other_view: {
+    display: "flex",
+    width: "95%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "2%",
+    //border: "1px solid white",
+  },
+  icon_view: {
+    marginLeft: "2%",
+  },
+};
