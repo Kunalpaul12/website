f4770307f9319cd275a6df8e7a48235fb2f01dc6
kunal <kunalp@invezta.com>
2020-04-05 12:09:17 +0530

    border to content

--- src/screens/skills/view.js
+++ src/screens/skills/view.js
@@ -5,6 +5,7 @@ import {
   faGlobe,
   faCode,
   faFolder,
+  faCogs,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faReact,
@@ -15,7 +16,7 @@ import {
   faCss3,
   faSass,
   faBootstrap,
-  faNodeJs,
+  faNode,
   faAngular,
   faJs,
   faJava,
@@ -26,6 +27,8 @@ import {
   faGitlab,
   faGithub,
   faBitbucket,
+  faJenkins,
+  faJira,
 } from "@fortawesome/free-brands-svg-icons";
 import style from "./style";
 
@@ -35,140 +38,187 @@ class Skills extends React.Component {
       <div style={style.container}>
         <p style={style.titile_text}>Skils That i learn</p>
         <div style={style.content_web_view}>
-          <FontAwesomeIcon color="white" size="2x" icon={faGlobe} />
-          <p style={{ color: "white", marginLeft: "1%" }}> Web</p>
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faReact}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faAngular}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faHtml5}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faCss3}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faSass}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faBootstrap}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faNodeJs}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faAws}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faDocker}
-          />
+          <div style={style.inner_tittle_view}>
+            <FontAwesomeIcon color="white" size="2x" icon={faGlobe} />
+            <p style={{ color: "white", marginLeft: "1%" }}> Web</p>
+          </div>
+
+          <div
+            style={Object.assign({}, style.inner_icon_view, { width: "40%" })}
+          >
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faReact}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faAngular}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faHtml5}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faCss3}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faSass}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faBootstrap}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faNode}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faAws}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faDocker}
+            />
+          </div>
         </div>
         <div style={style.content_other_view}>
-          <FontAwesomeIcon color="white" size="2x" icon={faMobileAlt} />
-          <p style={{ color: "white", marginLeft: "1%" }}> Mobile</p>
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faReact}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faAndroid}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faApple}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faUnity}
-          />
+          <div style={style.inner_tittle_view}>
+            <FontAwesomeIcon color="white" size="2x" icon={faMobileAlt} />
+            <p style={{ color: "white", marginLeft: "1%" }}> Mobile</p>
+          </div>
+          <div
+            style={Object.assign({}, style.inner_icon_view, { width: "15%" })}
+          >
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faReact}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faAndroid}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faApple}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faUnity}
+            />
+          </div>
         </div>
         <div style={style.content_other_view}>
-          <FontAwesomeIcon color="white" size="2x" icon={faCode} />
-          <p style={{ color: "white", marginLeft: "1%" }}> Language</p>
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faJs}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faJava}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faSwift}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faPython}
-          />
+          <div style={style.inner_tittle_view}>
+            <FontAwesomeIcon color="white" size="2x" icon={faCode} />
+            <p style={{ color: "white", marginLeft: "1%" }}> Language</p>
+          </div>
+          <div
+            style={Object.assign({}, style.inner_icon_view, { width: "15%" })}
+          >
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faJs}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faJava}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faSwift}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faPython}
+            />
+          </div>
         </div>
         <div style={style.content_other_view}>
-          <FontAwesomeIcon color="white" size="2x" icon={faFolder} />
-          <p style={{ color: "white", marginLeft: "1%" }}> Repository</p>
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faGitlab}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faGithub}
-          />
-          <FontAwesomeIcon
-            style={style.icon_view}
-            color="white"
-            size="2x"
-            icon={faBitbucket}
-          />
+          <div style={style.inner_tittle_view}>
+            <FontAwesomeIcon color="white" size="2x" icon={faFolder} />
+            <p style={{ color: "white", marginLeft: "1%" }}> Repository</p>
+          </div>
+          <div
+            style={Object.assign({}, style.inner_icon_view, { width: "12%" })}
+          >
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faGitlab}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faGithub}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faBitbucket}
+            />
+          </div>
+        </div>
+        <div style={style.content_other_view}>
+          <div style={style.inner_tittle_view}>
+            <FontAwesomeIcon color="white" size="2x" icon={faCogs} />
+            <p style={{ color: "white", marginLeft: "1%" }}> Testing</p>
+          </div>
+          <div
+            style={Object.assign({}, style.inner_icon_view, { width: "6%" })}
+          >
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faJenkins}
+            />
+            <FontAwesomeIcon
+              style={style.icon_view}
+              color="white"
+              size="2x"
+              icon={faJira}
+            />
+          </div>
         </div>
       </div>
     );
