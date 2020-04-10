a851fee88f8f96ea1673cfc0fd9bd8b85535b9c0
kunal <kunalp@invezta.com>
2020-04-10 17:38:37 +0530

    contact ui fix

--- src/screens/contact/view.js
+++ src/screens/contact/view.js
@@ -5,7 +5,6 @@ import { Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import "./style.scss";
 import emailjs from "emailjs-com";
-import Spinner from "react-bootstrap/Spinner";
 import config from "../../config/email";
 import { validator } from "./validator";
 import contact from "../../config/contact";
@@ -52,62 +51,62 @@ class Contact extends React.Component {
           <br />
           and I’ll get back or {contact.phone}.
         </p>
-        <div style={style.name_email_view}>
-          <Form.Group as={Col} md="6" controlId="formGroupName">
-            <Form.Label style={{ color: "white" }}>Name</Form.Label>
-            <Form.Control
-              type="text"
-              value={name}
-              onChange={(v) => {
-                name = v.target.value;
-                this.setState({ name });
-              }}
-            />
-          </Form.Group>
-          <Form.Group as={Col} md="6" controlId="formGroupEmail">
-            <Form.Label style={{ color: "white" }}>Email</Form.Label>
-            <Form.Control
-              type="email"
-              value={email}
-              onChange={(v) => {
-                email = v.target.value;
-                this.setState({ email });
-              }}
-            />
-          </Form.Group>
-        </div>
-        <div style={style.name_email_view}>
-          <Form.Group as={Col} md="12" controlId="exampleForm.ControlTextarea1">
-            <Form.Label style={{ color: "white" }}>How can i Help?</Form.Label>
-            <Form.Control
-              value={message}
-              as="textarea"
-              size="lg"
-              rows="4"
-              onChange={(v) => {
-                message = v.target.value;
-                this.setState({ message });
-              }}
-            />
-          </Form.Group>
-        </div>
-        <div style={style.name_email_view}>
-          <Button
-            style={style.button}
-            onClick={() => {
-              this.send();
-            }}
-          >
-            Send Message
-            {spinner && (
-              <Spinner
-                variant="light"
-                animation="border"
-                size="sm"
-                style={style.spinner}
+        <div style={style.formView}>
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label style={{ color: "white" }}>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={name}
+                  onChange={(v) => {
+                    name = v.target.value;
+                    this.setState({ name });
+                  }}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label style={{ color: "white" }}>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(v) => {
+                    email = v.target.value;
+                    this.setState({ email });
+                  }}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1">
+              <Form.Label style={{ color: "white" }}>
+                How can i Help ?
+              </Form.Label>
+              <Form.Control
+                value={message}
+                as="textarea"
+                size="lg"
+                rows="4"
+                onChange={(v) => {
+                  message = v.target.value;
+                  this.setState({ message });
+                }}
               />
-            )}
-          </Button>
+            </Form.Group>
+            <Form.Group>
+              <Col sm={{ span: 10, offset: 4 }}>
+                <Button
+                  style={style.button}
+                  onClick={() => {
+                    this.send();
+                  }}
+                >
+                  {spinner ? "Sending ..." : "Submit"}
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
         </div>
       </div>
     );
