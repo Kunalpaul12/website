db3feff4ffabbfc25f9420c96f0478f9961cae5b
kunal <kunalp@invezta.com>
2020-04-11 17:46:47 +0530

    info project screen

---
+++ src/public/screen/project/index.js
@@ -0,0 +1,30 @@
+export default {
+  mobile: {
+    fino:
+      "A cross-platform application made in react-native. It allows mutual fund managers to invest in mutual funds and shares. It also has a robo advisitory that machine lean the market and suggest the mutual fund and scheme favourable to you as per yours age ,target and income",
+    invezta:
+      "A cross-platform application made in react-native. It allows users to invest in mutual funds and shares. It has a customized dashboard for all user where they can buy ,rebuy or sell the shares that has been purchase and have day to day market data for better suggestions",
+    firstblood:
+      "A native application made in android studio. It allows user to participate in an pubg tournament by creating a team, solo or squad and win exciting prize depend upon number of kills, point and lot others factors. User can cash out the point earn by paytm or google pay",
+    kanopi:
+      "A cross-platform application made in react-native. It allows medical representative to collected and update data from an IOT device over wifi as well as send the data to server for analytical and reporting purpose. Base on data patient education such as video ,pdf etc are push into devices",
+    medempower:
+      "A cross-platform application made in react-native. It allows doctors to solve medical issues related to law by an expert team of lawyers. Services provided are talk to lawyer ,chat to lawyer ,appointment with lawyer depending upon the urgency of the condition",
+    akuna:
+      "A native application made in xcode. It allows medical representative to host a check-up camp where number of patient data related to medical history and there report are send to server. Base of the medical report generated pharma company can target there patient accordingly",
+    cipla:
+      "A native application made in unity. It allows medical representative to explain the detail information of a medicine journey. From entering to the body to the execution and impact of it on body through augmented and virtual reality and also to generate medical awareness ",
+  },
+  web: {
+    firstblood:
+      "A full-stack application made in react ,node and mongodb. It allow admin to host a tournament on pug server and send their information to all the user who are participating via notification using google notification and analytics, using technology such as ocr from aws to read screen shot data send by the users",
+    kanopi:
+      "A full-stack application made in react ,node ,mongodb ,bootstrap ,html5 ,sass. It allows the admin to push data to an IOT device remotely depending upon the time zone the cron job is processed. An admin can also get specific data of a content that has maximum view and download. It can anytime activate and deactivate a users ",
+    medempower:
+      "A full-stack application made in react ,node ,mongodb ,bootstrap ,html5 ,sass. It allows the admin to send notification to all the lawyers about the new query and request to process it as soon as possible. Providing encryption chat services. Automating the process of top FAQ of the month and best solution provided",
+    akuna:
+      "A full-stack application made in react ,node ,mongodb ,bootstrap ,html5 ,sass. It allows the admin to find out reports of any patient specific to a disease which can help in development of the new medicine and drugs. Automating the process of dispatching the medicine to its respective medical representative",
+    cipla:
+      "A full-stack application made in react ,node ,mongodb ,bootstrap ,html5 ,sass ,docker .It allows  admin to generate augmented and virtual reality application which can respond to real world object and help to explain about it in an 3d way",
+  },
+};
--- src/screens/project/view.js
+++ src/screens/project/view.js
@@ -1,24 +1,12 @@
 import React from "react";
 import CardColumns from "react-bootstrap/CardColumns";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMobileAlt, faGlobe } from "@fortawesome/free-solid-svg-icons";
-import {
-  faReact,
-  faAndroid,
-  faApple,
-  faUnity,
-  faHtml5,
-  faCss3,
-  faSass,
-  faBootstrap,
-  faNodeJs,
-  faAngular,
-  faAws,
-  faDocker,
-} from "@fortawesome/free-brands-svg-icons";
+import * as faSolid from "@fortawesome/free-solid-svg-icons";
+import * as faBrand from "@fortawesome/free-brands-svg-icons";
 import Image from "../../img/mobile";
 import CardView from "./component";
 import style from "./style";
+import Des from "../../public/screen/project";
 
 class Project extends React.Component {
   constructor(props) {
@@ -57,7 +45,11 @@ class Project extends React.Component {
               width: innerWidth < 900 ? innerWidth - innerWidth / 15 : "85%",
             })}
           >
-            <FontAwesomeIcon color="white" size="2x" icon={faMobileAlt} />
+            <FontAwesomeIcon
+              color="white"
+              size="2x"
+              icon={faSolid.faMobileAlt}
+            />
             <p style={style.mobile_header_text}>Mobile</p>
           </div>
 
@@ -70,77 +62,48 @@ class Project extends React.Component {
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? "lg" : "2x"}
-              icon={faReact}
+              icon={faBrand.faReact}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? "lg" : "2x"}
-              icon={faAndroid}
+              icon={faBrand.faAndroid}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? "lg" : "2x"}
-              icon={faApple}
+              icon={faBrand.faApple}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? "lg" : "2x"}
-              icon={faUnity}
+              icon={faBrand.faUnity}
             />
           </div>
         </div>
         <CardColumns>
-          <CardView
-            img={Image.Fino}
-            title="Fino"
-            des=" This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer."
-          />
+          <CardView img={Image.Fino} title="Fino" des={Des.mobile.fino} />
           <CardView
             img={Image.Invezta}
             title="Invezta"
-            des=" This card has supporting text below as a natural lead-in to
-            additional content."
+            des={Des.mobile.invezta}
           />
           <CardView
             img={Image.Firstblood}
             title="Firstblood"
-            des="  This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
+            des={Des.mobile.firstblood}
           />
-          <CardView
-            img={Image.Kanopi}
-            title="Kanopi"
-            des="  This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
-          />
+          <CardView img={Image.Kanopi} title="Kanopi" des={Des.mobile.kanopi} />
           <CardView
             img={Image.Medempower}
             title="Medempower"
-            des="   This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
+            des={Des.mobile.medempower}
           />
-          <CardView
-            img={Image.Akuna}
-            title="Akuna"
-            des="This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
-          />
-          <CardView
-            img={Image.Cipla}
-            title="Cipla"
-            des="This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
-          />
+          <CardView img={Image.Akuna} title="Akuna" des={Des.mobile.akuna} />
+          <CardView img={Image.Cipla} title="Cipla" des={Des.mobile.cipla} />
         </CardColumns>
         <div style={style.mobile_header_view}>
           <div
@@ -148,7 +111,7 @@ class Project extends React.Component {
               width: innerWidth < 1250 ? innerWidth - innerWidth / 5 : "75%",
             })}
           >
-            <FontAwesomeIcon color="white" size="2x" icon={faGlobe} />
+            <FontAwesomeIcon color="white" size="2x" icon={faSolid.faGlobe} />
             <p style={style.mobile_header_text}>Web </p>
           </div>
 
@@ -161,55 +124,55 @@ class Project extends React.Component {
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? "lg" : "2x"}
-              icon={faReact}
+              icon={faBrand.faReact}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? "lg" : "2x"}
-              icon={faAngular}
+              icon={faBrand.faAngular}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faHtml5}
+              icon={faBrand.faHtml5}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faCss3}
+              icon={faBrand.faCss3}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faSass}
+              icon={faBrand.faSass}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faBootstrap}
+              icon={faBrand.faBootstrap}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faNodeJs}
+              icon={faBrand.faNodeJs}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faAws}
+              icon={faBrand.faAws}
             />
             <FontAwesomeIcon
               style={style.fontawesome_view}
               color="white"
               size={innerWidth < 650 ? (innerWidth < 361 ? "1x" : "lg") : "2x"}
-              icon={faDocker}
+              icon={faBrand.faDocker}
             />
           </div>
         </div>
@@ -217,38 +180,16 @@ class Project extends React.Component {
           <CardView
             img={Image.Firstblood}
             title="Firstblood"
-            des="  This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
+            des={Des.web.firstblood}
           />
-          <CardView
-            img={Image.Kanopi}
-            title="Kanopi"
-            des="  This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
-          />
+          <CardView img={Image.Kanopi} title="Kanopi" des={Des.web.kanopi} />
           <CardView
             img={Image.Medempower}
             title="Medempower"
-            des="   This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
+            des={Des.web.medempower}
           />
-          <CardView
-            img={Image.Akuna}
-            title="Akuna"
-            des="This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
-          />
-          <CardView
-            img={Image.Cipla}
-            title="Cipla"
-            des="This is a wider card with supporting text below as a natural
-            lead-in to additional content. This card has even longer content
-            than the first to show that equal height action."
-          />
+          <CardView img={Image.Akuna} title="Akuna" des={Des.web.akuna} />
+          <CardView img={Image.Cipla} title="Cipla" des={Des.web.cipla} />
         </CardColumns>
       </div>
     );
