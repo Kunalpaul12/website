eb737d1fb437d17e18fce9b722dc7a9959ce9e3e
kunal <kunalp@invezta.com>
2020-04-11 21:14:05 +0530

    Updates

---
+++ precache-manifest.c790be4eaeb4193dc2d3f3f9ac07f369.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c966a72cf7204add1dbe0492a0a8e86f",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4df384676cebdca7a786",
+    "url": "/website/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "13cf966c79d1d923f5b1",
+    "url": "/website/static/css/main.0b6328c0.chunk.css"
+  },
+  {
+    "revision": "4df384676cebdca7a786",
+    "url": "/website/static/js/2.fbc5715b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.fbc5715b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cf966c79d1d923f5b1",
+    "url": "/website/static/js/main.2e6cc94d.chunk.js"
+  },
+  {
+    "revision": "c0e7a96e1e1fafc71966",
+    "url": "/website/static/js/runtime-main.c0bafae9.js"
+  },
+  {
+    "revision": "335f557f71b4d96cf0b65a3446af75b6",
+    "url": "/website/static/media/akuna.335f557f.png"
+  },
+  {
+    "revision": "41bc8c3530141b85ae831b2f10edba3a",
+    "url": "/website/static/media/cipla.41bc8c35.png"
+  },
+  {
+    "revision": "37bf7ef247b7bec312059be81cd97eff",
+    "url": "/website/static/media/kanopi.37bf7ef2.png"
+  },
+  {
+    "revision": "75aaf1934d0d709301667bb675da5d27",
+    "url": "/website/static/media/medempower.75aaf193.jpg"
+  },
+  {
+    "revision": "a6cd516a1be2c7d4baf6faf78c631d1a",
+    "url": "/website/static/media/user.a6cd516a.png"
+  }
+]);
